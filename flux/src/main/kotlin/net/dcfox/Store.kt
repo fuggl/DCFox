ac244d001c4f7290168a34934fd75028f39bb326
sebastian <fuchs.se.de@gmail.com>
2018-01-24 18:02:55 +0100

    added access key to properties (write access exception)

--- flux/src/main/kotlin/net/dcfox/Store.kt
+++ flux/src/main/kotlin/net/dcfox/Store.kt
@@ -1,36 +1,35 @@
 package net.dcfox
 
 sealed class AccessKey {
-    fun isAssociatedWith(otherKey: AccessKey) : Boolean {
-        if (this !== otherKey) {
-            //TODO: throw access exception
-            return false
-        }
+    fun isAssociatedWith(otherKey: AccessKey): Boolean {
+        if (this !== otherKey) throw WriteAccessException()
         return true
     }
     companion object NoKey : AccessKey()
 }
 
+class WriteAccessException(): Exception()
+
 open class Store(protected val accessKey: AccessKey = AccessKey.NoKey) {
     protected fun set(property: ReadOnlyBoolean, value: Boolean) {
-        property.set(value)
+        property.set(value, accessKey)
     }
     protected fun set(property: ReadOnlyDouble, value: Double) {
-        property.set(value)
+        property.set(value, accessKey)
     }
     protected fun set(property: ReadOnlyFloat, value: Float) {
-        property.set(value)
+        property.set(value, accessKey)
     }
     protected fun set(property: ReadOnlyInteger, value: Int) {
-        property.set(value)
+        property.set(value, accessKey)
     }
     protected fun set(property: ReadOnlyLong, value: Long) {
-        property.set(value)
+        property.set(value, accessKey)
     }
     protected fun <T>set(property: ReadOnlyObject<T>, value: T) {
         property.set(value, accessKey)
     }
     protected fun set(property: ReadOnlyString, value: String) {
-        property.set(value)
+        property.set(value, accessKey)
     }
 }
