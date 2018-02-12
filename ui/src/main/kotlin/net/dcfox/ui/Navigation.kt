611d3a65dd8370a4de66c5bf79107f51f7feb5ea
sebastian <fuchs.se.de@gmail.com>
2018-02-12 19:41:21 +0100

    added navigation view

---
+++ ui/src/main/kotlin/net/dcfox/ui/Navigation.kt
@@ -0,0 +1,15 @@
+package net.dcfox.ui
+
+import tornadofx.*
+
+class Navigation: View() {
+    override val root = vbox {
+
+    }
+}
+
+class PlantStructre: View() {
+    override val root = treeview<String> {
+
+    }
+}
