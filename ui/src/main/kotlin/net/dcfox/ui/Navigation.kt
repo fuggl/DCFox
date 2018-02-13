5d86d0a5a97a043e62fa15ad53367dee71d817d3
sebastian <fuchs.se.de@gmail.com>
2018-02-13 17:39:31 +0100

    added control and station structure to navigation view

--- ui/src/main/kotlin/net/dcfox/ui/Navigation.kt
+++ ui/src/main/kotlin/net/dcfox/ui/Navigation.kt
@@ -1,15 +1,30 @@
 package net.dcfox.ui
 
+import javafx.scene.Parent
 import tornadofx.*
 
 class Navigation: View() {
     override val root = vbox {
+        add(PlantStructure())
+        tabpane {
+            tab(ControllerStructure())
+            tab(StationStructure())
+        }
+    }
+}
+
+class PlantStructure: View() {
+    override val root = treeview<String> {
 
     }
 }
 
-class PlantStructre: View() {
-    override val root = treeview<String> {
+class ControllerStructure: View() {
+    override val root: Parent
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+}
 
-    }
+class StationStructure: View() {
+    override val root: Parent
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
 }
